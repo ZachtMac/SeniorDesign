1a2604abca1c03161904d716371e17a3891b994f
Jamernacht99 <lamjs@mail.uc.edu>
2023-11-07 19:57:50 -0500

    ui fix

---
+++ GearNet/Controllers/InformationController.cs
@@ -0,0 +1,32 @@
+﻿using GearNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GearNet.Controllers
+{
+    public class InformationController : Controller
+    {
+        private readonly ILogger<InformationController> _logger;
+
+        public InformationController(ILogger<InformationController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
